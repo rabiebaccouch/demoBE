af4eab952b32e5d3bb77da28d075df71924df24c
rabiebaccouch <rabiebaccouch7@gmail.com>
2022-08-26 12:08:41 +0200

    fix cors problem

--- src/main/java/com/csf/demo/controller/ArticleController.java
+++ src/main/java/com/csf/demo/controller/ArticleController.java
@@ -1,7 +1,8 @@
 package com.csf.demo.controller;
 
 import java.util.List;  
-import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;  
 import org.springframework.web.bind.annotation.GetMapping;  
 import org.springframework.web.bind.annotation.PathVariable;  
@@ -16,6 +17,7 @@ import com.csf.demo.service.ArticleService;
  
 //mark class as Controller  
 @RestController  
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ArticleController   
 {  
 	//autowire the BooksService class  
